fa3e4cc948fc1e758f4dd987e0e1ddd8d8d5ab59
JamesSweetland <56346421+JamesSweetland@users.noreply.github.com>
2020-01-21 14:16:33 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var app = angular.module('myApp', ['ui.router']);
+
+app.controller('myCtrl', function($scope, $state) {
+  $scope.val = "This is my Message...";
+  $scope.views = ['File1', 'File2', 'File3'];
+  $scope.Model = {
+    selected: null
+  };
+  $scope.go = function(index) {
+    $state.go('view_route', {
+      index: index
+    });
+  };
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('view/route1', {
+      url: "/route1",
+      templateUrl: "file1.html"
+    })
+
+  .state('view_route', {
+    url: "/route/{index:int}",
+    templateProvider: function($templateRequest, $stateParams) {
+
+      var index = $stateParams.index + 1;
+      var templateName = 'file' + index + '.html';
+
+      return $templateRequest(templateName);
+    },
+  });
+
+});
